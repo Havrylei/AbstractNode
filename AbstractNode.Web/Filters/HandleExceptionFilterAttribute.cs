871d36aeb6c2e1f928c97bb2eeb0c221497ca322
Havrylei Petro <havrylei.petro@gmail.com>
2018-02-08 17:19:03 +0200

    #5

---
+++ AbstractNode.Web/Filters/HandleExceptionFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ClinicApi.Resources.Filters
+{
+    /// <summary>
+    ///     Handle Exception Filter Attribute
+    /// </summary>
+    public class HandleExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        /// <summary>
+        ///     On Exception
+        /// </summary>
+        public override void OnException(ExceptionContext context)
+        {
+            context.Result = new BadRequestObjectResult("Bad Request\n" + context.Exception.Message);
+        }
+    }
+}
---
+++ AbstractNode.Web/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClinicApi.Resources.Filters
+{
+    /// <summary>
+    ///     Validate Model Attribute
+    /// </summary>
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        ///     On Action Executing
+        /// </summary>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid)
+            {
+                return;
+            }
+
+            Dictionary<string, string> errors = context.ModelState
+                .Where(x => x.Value.Errors.Any())
+                .ToDictionary(key => key.Key, value => value.Value.Errors.First().ErrorMessage);
+
+            context.Result = new BadRequestObjectResult(errors);
+        }
+    }
+}
